c7a193376eb75275022e38b5cfaf05949ae9ad37
gustavo <g.seijas1305@gmail.com>
2018-08-09 15:59:57 -0600

    primer modificacion

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>KIKITO DESCAMPS!</Text>
+        <Text>CURSO DE REACT NATIVE </Text>     
+        <Text> QUE TAL </Text> 
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
